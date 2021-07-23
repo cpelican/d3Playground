fc7ab414840f3607a61ebd9fb58727365a4ace3c
carolinepellet <pellet@spaziodati.eu>
2021-07-23 14:28:04 +0200

    the zoom is in its own function and remove some warnings

--- src/StellarBodiesApp/StellarBodies.tsx
+++ src/StellarBodiesApp/StellarBodies.tsx
@@ -1,13 +1,8 @@
 import {useEffect, useRef} from 'react';
-import * as d3 from 'd3';
-import * as d3Zoom from 'd3-zoom';
 import React from 'react';
-import {buildPlanetsOnOrbits, buildSun} from './svgBuilders';
+import {buildPlanetsOnOrbits, buildSun, buildZoom, changeViewBox} from './svgBuilders';
 import {StellarBody} from './StellarBodiesTypes';
 import {svg} from 'd3-fetch';
-import {BaseType, ZoomedElementBaseType} from 'd3';
-
-type D3CallArg = (selection: d3.Selection<BaseType, unknown, HTMLElement, unknown>, ...args: any[]) => void;
 
 interface StellarBodiesProps {
     sun: StellarBody | null;
@@ -32,17 +27,6 @@ export const StellarBodies = ({
 }: StellarBodiesProps) => {
     const svgRef = useRef(null);
 
-    function zoomedCanvas(this: BaseType, event: any, d: unknown) {
-        let svgElement = this as SVGSVGElement,
-            oldWidth = parseInt(width),
-            oldHeight = parseInt(heigth),
-            viewBoxWidth = oldWidth * event.transform.k,
-            viewBoxHeight = oldHeight * event.transform.k,
-            x = (oldWidth - viewBoxWidth) / 2,
-            y = (oldHeight - viewBoxHeight) / 2;
-        d3.select(svgElement).attr('viewBox', `${x} ${y} ${viewBoxWidth} ${viewBoxHeight}`);
-    }
-
     useEffect(() => {
         let svgElement: SVGSVGElement | null = svgRef?.current,
             hasAllStellarBodies = sun != null && (planets?.length ?? 0) > 0;
@@ -53,16 +37,10 @@ export const StellarBodies = ({
                 sunRadius = sunElement?.r.animVal?.value ?? 0;
 
             buildPlanetsOnOrbits(svgElement, planets, sunRadius, scale, rotationSpeed);
+
+            let zoomCallBack = changeViewBox(parseInt(width), parseInt(heigth));
+            buildZoom(svgElement, minZoom, maxZoom, zoomCallBack)
         }
-
-        let svgZoom: d3Zoom.ZoomBehavior<ZoomedElementBaseType, unknown>;
-
-        svgZoom = d3Zoom
-            .zoom<ZoomedElementBaseType, unknown>()
-            .on('zoom', zoomedCanvas)
-            .scaleExtent([minZoom, maxZoom]);
-
-        d3.select('svg').call(svgZoom as D3CallArg);
     }, [planets, sun, scale]);
 
     return (
--- src/StellarBodiesApp/svgBuilders.ts
+++ src/StellarBodiesApp/svgBuilders.ts
@@ -7,6 +7,11 @@ import {
     computeSvgComputedDimensions,
     computeSvgContainerCenter,
 } from './utils';
+import * as d3Zoom from 'd3-zoom';
+import {ZoomedElementBaseType} from 'd3';
+import {BaseType} from 'd3';
+
+type D3CallArg = (selection: d3.Selection<SVGSVGElement, unknown, null, unknown>, ...args: any[]) => void;
 
 export function buildSun(container: SVGSVGElement, data: StellarBody, scale: number) {
     let [centerX, centerY] = computeSvgContainerCenter(container),
@@ -67,6 +72,30 @@ function buildAnimation(d: StellarBody, rotationSpeed: number) {
         .ease(d3.easeLinear)
         .attrTween('transform', () => d3.interpolateString('rotate(0)', 'rotate(360)'))
         .on('end', (d: unknown) => buildAnimation(d as StellarBody, rotationSpeed));
+}
+
+export function buildZoom(container: SVGSVGElement, minZoom: number, maxZoom: number, callBack: (this: BaseType, event: any) => void): void {
+    let svgZoom: d3Zoom.ZoomBehavior<ZoomedElementBaseType, unknown>;
+
+        svgZoom = d3Zoom
+            .zoom<ZoomedElementBaseType, unknown>()
+            .on('zoom', callBack)
+            .scaleExtent([minZoom, maxZoom]);
+
+        d3.select(container).call(svgZoom as unknown as D3CallArg);
+}
+
+export function changeViewBox(width: number, heigth: number) {
+    return function(this: BaseType, event: any): void {
+        let svgElement = this as SVGSVGElement,
+            oldWidth = width,
+            oldHeight = heigth,
+            viewBoxWidth = oldWidth * event.transform.k,
+            viewBoxHeight = oldHeight * event.transform.k,
+            x = (oldWidth - viewBoxWidth) / 2,
+            y = (oldHeight - viewBoxHeight) / 2;
+        d3.select(svgElement).attr('viewBox', `${x} ${y} ${viewBoxWidth} ${viewBoxHeight}`);
+    }
 }
 
 function createClassName(d: StellarBody): string {
